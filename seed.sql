37d33a2b20d00e2e1a9b23a409ece8fd79ee3d80
dp <davepollan@gmail.com>
2020-02-18 23:54:59 -0500

    database constructor done

--- seed.sql
+++ seed.sql
@@ -24,3 +24,20 @@ CREATE TABLE employee(
   manager_id INTEGER(11),
   PRIMARY KEY (id)
 );
+
+INSERT INTO department (dept_name)
+VALUES ("Sales"), ("Finance"), ("Legal"), ("Engineering"), ("Management");
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1), ("Sales Person", 80000, 1);
+VALUES ("Accountant", 100000, 2);
+VALUES ("Lawyer", 250000, 3), ("Legal Team Lead", 280000, 3);
+VALUES ("Jr. Engineer", 75000, 4), ("Software Engineer", 100000, 4), ("Lead Engineer", 130000, 4);
+VALUES ("Engineering Manager", 250000, 5), ("Sales Manager", 250000, 5);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, 5), ("Mike", "Chan", 2, 5), ("Max", "Harly", 4, 5), ("Ashley", "Rodriguez", 6, 5), ("Kevin", "Tupik", 5, 5), ("Malia", "Brown", 7, 5);
+VALUES ("Sarah", "Lord", 3), ("Tom", "Allen", 3);
+
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3500
-  port: 3500,
+  // Your port; if not 3000
+  port: 3306,
 
   // Your username
   user: "root",
@@ -22,3 +22,39 @@ connection.connect(function(err) {
     // run the start function after the connection is made to prompt the user
     start();
   });
+
+
+  // function which prompts the user for what action they should take
+function start() {
+    inquirer
+    .prompt({
+      name: "chooseAction",
+      type: "list",
+      message: "Would you like to do? (use arrow keys)",
+      choices: ["View all employees", "View all employees by department", "View all employees by manager",
+      "Add employee", "Remove Employee", "Update employee role", "Update Employee manager", "exit"]
+    })
+    .then(function(answer) {
+        // based on their answer, either call the bid or the post functions
+        if (answer.chooseAction === "View all employees") {
+          viewAll();
+        }
+        else if(answer.postOrBid === "View all employees by department") {
+            viewAllByDept();
+        } else{
+          connection.end();
+        }
+      });
+}
+
+function viewAll() {
+    connection.query(
+        "INSERT INTO auctions SET ?",
+        {
+          item_name: answer.item,
+          category: answer.category,
+          starting_bid: answer.startingBid || 0,
+          highest_bid: answer.startingBid || 0
+        }
+      );
+}
